f81da4b99f1dbbab33dcc0c62b54a270280ded0b
Lukas Korbitsch <korbitschl@gmail.com>
2025-06-03 12:49:18 +0200

    updated routes, added dependencies, added googel maps api

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,18 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CatchedController;
+use App\Http\Controllers\DashboardController;
 
 Route::redirect('/', '/dashboard');
 
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified',])->group(function () {
 
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('catched', CatchedController::class)->except(['update']);
+    Route::post('catched/{catched}/update', [CatchedController::class, 'update'])->name('catched.update');
+    Route::delete('catched/photo/{mediaId}', [CatchedController::class, 'deletePhoto'])
+    ->middleware(['auth', 'verified'])
+    ->name('catched.photo.delete');
+
 });
