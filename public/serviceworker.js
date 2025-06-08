c2724714a410f6089e32bed8fb3c3921dcab304f
Lukas Korbitsch <korbitschl@gmail.com>
2025-06-08 16:34:06 +0200

    corrected serviceworker

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,16 +1,12 @@
 var staticCacheName = "pwa-v" + new Date().getTime();
 var filesToCache = [
     '/offline',
-    '/css/app.css',
-    '/js/app.js',
-    '/images/icons/icon-72x72.png',
-    '/images/icons/icon-96x96.png',
-    '/images/icons/icon-128x128.png',
-    '/images/icons/icon-144x144.png',
-    '/images/icons/icon-152x152.png',
-    '/images/icons/icon-192x192.png',
-    '/images/icons/icon-384x384.png',
-    '/images/icons/icon-512x512.png',
+    '/images/icons/logo-72.png',
+    '/images/icons/logo-96.png',
+    '/images/icons/logo-144.png',
+    '/images/icons/logo-152.png',
+    '/images/icons/logo-192.png',
+    '/images/icons/logo-512.png',
 ];
 
 // Cache on install
