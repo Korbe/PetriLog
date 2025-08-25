951203cb865932b8df052ca812967feecc18d9f8
Lukas Korbitsch <korbitschl@gmail.com>
2025-08-25 19:05:41 +0200

    Added enhancement for login to remember email

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -6,6 +6,9 @@ import Checkbox from '@/JetstreamComponents/Checkbox.vue';
 import VButton from '@/components/VButton.vue';
 import VInput from '@/components/VInput.vue';
 import VPassword from '@/components/VPassword.vue';
+import Cookies from 'js-cookie';
+import { ref, onMounted } from 'vue';
+import { PencilIcon } from '@heroicons/vue/24/solid';
 
 defineProps({
     canResetPassword: Boolean,
@@ -18,17 +21,37 @@ const form = useForm({
     remember: false,
 });
 
+const showEmailInput = ref(true);
+const savedEmail = ref('');
+
+onMounted(() => {
+    const emailFromCookie = Cookies.get('petriLoginEmail');
+    if (emailFromCookie) {
+        savedEmail.value = emailFromCookie;
+        showEmailInput.value = false;
+        form.email = emailFromCookie; // optional, falls du FormValue brauchst
+    }
+});
+
 const submit = () => {
     form.transform(data => ({
         ...data,
         remember: form.remember ? 'on' : '',
     })).post(route('login'), {
-        onFinish: () => form.reset('password'),
+        onFinish: () => {
+            form.reset('password');
+            Cookies.set('petriLoginEmail', form.email, { expires: 365, path: '/' });
+        },
     });
+};
+
+const editEmail = () => {
+    showEmailInput.value = true;
 };
 </script>
 
 <template>
+
     <Head title="Log in" />
 
     <AuthenticationCard>
@@ -42,29 +65,25 @@ const submit = () => {
 
         <form @submit.prevent="submit">
             <div>
-                <VInput
-                    label="Email"
-                    id="email"
-                    v-model="form.email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    required
-                    autofocus
-                    autocomplete="username"
-                    :error="form.errors.email"
-                />
+                <template v-if="showEmailInput">
+                    <VInput label="Email" id="email" v-model="form.email" type="email" class="mt-1 block w-full"
+                        required autofocus autocomplete="username" :error="form.errors.email" />
+                </template>
+
+                <template v-else>
+                    <div
+                        class="flex items-center justify-center mt-1 p-2 dark:bg-gray-800">
+                        <span class="text-gray-700 dark:text-gray-200 truncate">{{ savedEmail }}</span>
+                        <button type="button" class="ml-2 text-gray-500 hover:text-gray-700" @click="editEmail">
+                            <PencilIcon class="w-5 h-5" />
+                        </button>
+                    </div>
+                </template>
             </div>
 
             <div class="mt-4">
-                <VPassword
-                    label="Passwort"
-                    id="password"
-                    v-model="form.password"
-                    class="mt-1 block w-full"
-                    required
-                    autocomplete="current-password"
-                    :error="form.errors.password"
-                />
+                <VPassword label="Passwort" id="password" v-model="form.password" class="mt-1 block w-full" required
+                    autocomplete="current-password" :error="form.errors.password" />
             </div>
 
             <div class="block mt-4">
@@ -75,11 +94,8 @@ const submit = () => {
             </div>
 
             <div class="flex w-full justify-center mt-4">
-                <!-- <Link v-if="canResetPassword" :href="route('password.request')" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                    Passwort vergessen?
-                </Link> -->
-
-                <VButton type="submit" class="w-full" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                <VButton type="submit" class="w-full" :class="{ 'opacity-25': form.processing }"
+                    :disabled="form.processing">
                     Anmelden
                 </VButton>
             </div>
