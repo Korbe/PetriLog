83b25e9abd1d0c6989bc498ac18026c918a61a09
Lukas Korbitsch <korbitschl@gmail.com>
2025-09-27 17:00:42 +0200

    moved v-if

--- resources/js/Pages/Catched/Show.vue
+++ resources/js/Pages/Catched/Show.vue
@@ -122,8 +122,8 @@
         </div>
       </div>
 
-      <div class="bg-white dark:bg-gray-800 rounded-lg border-1 dark:border-0  mx-2 mt-5">
-        <div v-if="catched.remark" class=" w-full   p-5">
+      <div v-if="catched.remark" class="bg-white dark:bg-gray-800 rounded-lg border-1 dark:border-0  mx-2 mt-5">
+        <div class=" w-full p-5">
           <p>{{ catched.remark }}</p>
         </div>
       </div>
