88459d7e6d5e345acea62ae6c0dc60149c9561a0
Lukas Korbitsch <korbitschl@gmail.com>
2026-01-29 21:38:13 +0100

    uncomplete notice put on bottom

--- resources/js/Pages/Fish/Index.vue
+++ resources/js/Pages/Fish/Index.vue
@@ -5,16 +5,6 @@
       <VInput placeholder="Suche" v-model="search" />
     </template>
 
-    <div
-      class="flex items-center gap-2 rounded-xl bg-blue-50 border border-blue-200 text-blue-800 px-4 py-3 text-sm mt-2">
-      <span>
-        Diese Liste ist unvollständig und wird laufend ergänzt. Sollten Sie Vorschläge oder Ergänzungen haben schreiben
-        Sie
-        uns gerne per Mail
-        <a href="mailto:info@petrilog.com">info@petrilog.com</a>
-      </span>
-    </div>
-
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 pt-5">
       <div v-for="fish in filteredFish" :key="fish.id"
         class="bg-white dark:bg-gray-800 rounded-lg shadow hover:shadow-lg transition-shadow duration-200 overflow-hidden">
@@ -22,12 +12,23 @@
         <div class="relative w-full h-44">
           <img :src="fish.media[0]?.url || '/images/fish-default.png'" :alt="fish.name"
             class="w-full h-full object-contain hover:scale-105 transition-transform duration-200" />
-          <div class="absolute bottom-0 left-0 w-full bg-primary-500 bg-opacity-50 text-white text-center py-1 font-semibold">
+          <div
+            class="absolute bottom-0 left-0 w-full bg-primary-500 bg-opacity-50 text-white text-center py-1 font-semibold">
             {{ fish.name }}
           </div>
         </div>
         </Link>
       </div>
+    </div>
+
+    <div
+      class="flex items-center gap-2 rounded-xl bg-blue-50 border border-blue-200 text-blue-800 mt-5 px-4 py-3 text-sm mt-2">
+      <span>
+        Diese Liste ist unvollständig und wird laufend ergänzt. Sollten Sie Vorschläge oder Ergänzungen haben schreiben
+        Sie
+        uns gerne per Mail
+        <a href="mailto:info@petrilog.com">info@petrilog.com</a>
+      </span>
     </div>
   </PageWrapper>
 </template>
--- resources/js/Pages/States/Index.vue
+++ resources/js/Pages/States/Index.vue
@@ -1,43 +1,41 @@
 <template>
   <PageWrapper title="Gewässer" :backTo="`/dashboard`">
 
-    <div class="flex items-center gap-2 rounded-xl bg-blue-50 border border-blue-200 text-blue-800 px-4 py-3 text-sm">
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-10">
+      <div v-for="state in states" :key="state.id">
+        <Link :href="`/states/${state.id}`">
+        <div
+          class="bg-white dark:bg-gray-800 rounded-lg shadow hover:shadow-lg transition-shadow duration-200 overflow-hidden">
+
+          <!-- Bild -->
+          <div class="relative w-full h-44">
+            <img :src="state.media[0]?.url ?? '/placeholder.png'" :alt="state.name"
+              class="w-full h-full object-contain mt-5 hover:scale-105 transition-transform duration-200" />
+          </div>
+
+          <!-- Info-Bereich -->
+          <div class="p-4 flex flex-col items-center text-center">
+            <h2 class="text-xl font-bold">{{ state.name }}</h2>
+            <p class="text-gray-700 dark:text-gray-300 mt-2 text-sm">
+              {{ state.lakes_count }} {{ state.lakes_count === 1 ? 'See' : 'Seen' }} &nbsp;•&nbsp;
+              {{ state.rivers_count }} {{ state.rivers_count === 1 ? 'Fluss' : 'Flüsse' }}
+            </p>
+            <p class="text-gray-500 dark:text-gray-400 text-sm mt-1">
+              {{ state.associations_count }} {{ state.associations_count === 1 ? 'Verein' : 'Vereine' }}
+            </p>
+          </div>
+
+        </div>
+        </Link>
+      </div>
+    </div>
+
+    <div class="flex items-center gap-2 rounded-xl bg-blue-50 border border-blue-200 text-blue-800 mt-5  px-4 py-3 text-sm">
       <span>
         Diese Liste ist unvollständig und wird laufend ergänzt. Sollten Sie Vorschläge oder Ergänzungen haben,
         schreiben Sie uns gerne per Mail
         <a href="mailto:info@petrilog.com" class="underline">info@petrilog.com</a>
       </span>
-    </div>
-
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-10">
-      <div v-for="state in states" :key="state.id">
-        <Link :href="`/states/${state.id}`">
-          <div class="bg-white dark:bg-gray-800 rounded-lg shadow hover:shadow-lg transition-shadow duration-200 overflow-hidden">
-            
-            <!-- Bild -->
-            <div class="relative w-full h-44">
-              <img 
-                :src="state.media[0]?.url ?? '/placeholder.png'" 
-                :alt="state.name"
-                class="w-full h-full object-contain mt-5 hover:scale-105 transition-transform duration-200"
-              />
-            </div>
-
-            <!-- Info-Bereich -->
-            <div class="p-4 flex flex-col items-center text-center">
-              <h2 class="text-xl font-bold">{{ state.name }}</h2>
-              <p class="text-gray-700 dark:text-gray-300 mt-2 text-sm">
-                {{ state.lakes_count }} {{ state.lakes_count === 1 ? 'See' : 'Seen' }} &nbsp;•&nbsp;
-                {{ state.rivers_count }} {{ state.rivers_count === 1 ? 'Fluss' : 'Flüsse' }}
-              </p>
-              <p class="text-gray-500 dark:text-gray-400 text-sm mt-1">
-                {{ state.associations_count }} {{ state.associations_count === 1 ? 'Verein' : 'Vereine' }}
-              </p>
-            </div>
-
-          </div>
-        </Link>
-      </div>
     </div>
 
   </PageWrapper>
@@ -49,10 +47,10 @@ import PageWrapper from '@/Layouts/Dashboard/PageWrapper.vue';
 import { onMounted } from 'vue';
 
 defineProps({
-    states: Array
+  states: Array
 });
 
 onMounted(() => {
-    sessionStorage.setItem('lastOrigin', window.location.href);
+  sessionStorage.setItem('lastOrigin', window.location.href);
 });
 </script>
