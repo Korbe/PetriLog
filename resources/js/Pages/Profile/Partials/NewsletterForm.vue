9c25603b6877ac9428f7060b22616ba9dc5231fa
Lukas Korbitsch <korbitschl@gmail.com>
2026-02-01 15:00:20 +0100

    fixed newsletter checkbox layout

--- resources/js/Pages/Profile/Partials/NewsletterForm.vue
+++ resources/js/Pages/Profile/Partials/NewsletterForm.vue
@@ -1,45 +1,62 @@
 <script setup>
-import { useForm } from '@inertiajs/vue3';
-import ActionMessage from '@/JetstreamComponents/ActionMessage.vue';
-import FormSection from '@/JetstreamComponents/FormSection.vue';
-import PrimaryButton from '@/JetstreamComponents/PrimaryButton.vue';
-import VCheckbox from '@/components/VCheckbox.vue';
+import { useForm } from '@inertiajs/vue3'
+import ActionMessage from '@/JetstreamComponents/ActionMessage.vue'
+import PrimaryButton from '@/JetstreamComponents/PrimaryButton.vue'
+import VCheckbox from '@/components/VCheckbox.vue'
 
 const props = defineProps({
-    user: Object,
-});
+    user: {
+        type: Object,
+        required: true,
+    },
+})
 
 const form = useForm({
-    newsletter_opt_out: props.user.newsletter_opt_out
-});
+    newsletter_opt_out: props.user.newsletter_opt_out,
+})
 
-const updateNewsletterPreferences = () => {
-    form.patch(route('user-newsletter-preferences.update'));
-};;
+const submit = () => {
+    form.patch(route('user-newsletter-preferences.update'), {
+        preserveScroll: true,
+    })
+}
 </script>
 
 <template>
-    <FormSection @submitted="updateNewsletterPreferences">
-        <template #header>
-            <h1 class="text-lg font-medium text-gray-800 dark:text-gray-100">Newsletter</h1>
-            <p class="mt-1 mb-5 text-sm text-gray-600 dark:text-gray-300">Verwalten deine Newsletter-Einstellungen.</p>
-        </template>
+    <div class="md:grid md:grid-cols-2 md:gap-6">
+        <div class="mt-5 md:mt-0 md:col-span-2 rounded-lg bg-white dark:bg-gray-800">
+            <form @submit.prevent="submit">
+                <!-- CONTENT -->
+                <div class="px-4 py-5 sm:p-6">
 
-        <template #form>
+                    <!-- HEADER -->
+                    <h1 class="text-lg font-medium text-gray-800 dark:text-gray-100">
+                        Newsletter
+                    </h1>
+                    <p class="mt-1 mb-5 text-sm text-gray-600 dark:text-gray-300">
+                        Verwalte deine Newsletter-Einstellungen.
+                    </p>
 
-            <VCheckbox label="Ich möchte keine Newsletter Mails mehr erhalten" v-model="form.newsletter_opt_out"
-                :error="form.errors.newsletter_opt_out"></VCheckbox>
+                    <!-- FORM -->
+                    <div class="mt-4">
+                        <div class="col-span-6">
+                            <VCheckbox label="Ich möchte keine Newsletter-Mails mehr erhalten"
+                                v-model="form.newsletter_opt_out" :error="form.errors.newsletter_opt_out" />
+                        </div>
+                    </div>
+                </div>
 
-        </template>
+                <!-- ACTIONS -->
+                <div class="rounded-lg flex items-center px-4 py-3 shadow">
+                    <ActionMessage :on="form.recentlySuccessful">
+                        Gespeichert.
+                    </ActionMessage>
 
-        <template #actions>
-            <ActionMessage :on="form.recentlySuccessful" class="me-3">
-                Gespeichert.
-            </ActionMessage>
-
-            <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                Speichern
-            </PrimaryButton>
-        </template>
-    </FormSection>
+                    <PrimaryButton type="submit" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                        Speichern
+                    </PrimaryButton>
+                </div>
+            </form>
+        </div>
+    </div>
 </template>
