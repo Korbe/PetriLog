9a844558bf4b9f96f3fc57742bf6cff173d36091
Lukas Korbitsch <korbitschl@gmail.com>
2026-02-07 15:11:40 +0100

    added additional fields to user table in admin section

--- resources/js/Pages/Admin/User/Index.vue
+++ resources/js/Pages/Admin/User/Index.vue
@@ -10,8 +10,8 @@
                            dark:bg-gray-700 dark:text-gray-200" />
             </div>
 
-            <div class="mt-8 mx-5 overflow-x-auto">
-                <div class="bg-white dark:bg-gray-800 rounded-lg p-4">
+            <div class="mt-8 mx-5 overflow-x-auto bg-white dark:bg-gray-800 rounded-lg">
+                <div class="p-4">
                     <table class="min-w-full divide-y divide-gray-300">
                         <thead>
                             <tr>
@@ -24,12 +24,25 @@
                                 <th
                                     class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 dark:text-gray-400">
                                     E-Mail</th>
-                                <th
-                                    class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 dark:text-gray-400">
-                                    Admin</th>
+
                                 <th
                                     class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 dark:text-gray-400">
                                     Registriert am</th>
+                                <th
+                                    class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 dark:text-gray-400">
+                                    Bundesland</th>
+                                <th
+                                    class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 dark:text-gray-400">
+                                    Abonniert</th>
+                                <th
+                                    class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 dark:text-gray-400">
+                                    Läuft ab am</th>
+                                <th
+                                    class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 dark:text-gray-400">
+                                    Gekündigt/nie abonniert</th>
+                                <th
+                                    class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 dark:text-gray-400">
+                                    Admin</th>
                                 <th
                                     class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 dark:text-gray-400">
                                     Aktionen</th>
@@ -47,14 +60,31 @@
                                     {{ user.name }}</td>
                                 <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm text-gray-700 dark:text-gray-300">{{
                                     user.email }}</td>
+
+                                <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm text-gray-700 dark:text-gray-300">
+                                    {{ new Date(user.created_at).toLocaleDateString() }}
+                                </td>
+                                <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm text-gray-700 dark:text-gray-300">
+                                    {{ user.state }}
+                                </td>
+
+                                <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm text-gray-700 dark:text-gray-300">
+                                    <CheckIcon v-if="user.subscribed" class="h-5 w-5 text-green-600" />
+                                    <XMarkIcon v-else class="h-5 w-5 text-red-500" />
+                                </td>
+
+                                <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm text-gray-700 dark:text-gray-300">
+                                    {{ new Date(user.subscription_expires_at).toLocaleDateString() }}
+                                </td>
+                                <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm text-gray-700 dark:text-gray-300">
+                                    <CheckIcon v-if="user.canceled" class="h-5 w-5 text-green-600" />
+                                    <XMarkIcon v-else class="h-5 w-5 text-red-500" />
+                                </td>
+
                                 <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm text-gray-700 dark:text-gray-300">
                                     <CheckIcon v-if="user.isAdmin" class="h-5 w-5 text-green-600" />
                                     <XMarkIcon v-else class="h-5 w-5 text-red-500" />
                                 </td>
-                                <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm text-gray-700 dark:text-gray-300">
-                                    {{ new Date(user.created_at).toLocaleDateString() }}
-                                </td>
-
                                 <!-- Aktionen -->
                                 <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm">
                                     <VButton :href="route('admin.impersonate', user.id)" size="sm" variant="secondary"
@@ -87,6 +117,14 @@ interface User {
     email: string
     created_at: string
     isAdmin: boolean
+
+    // Subscription (Laravel Spark / Cashier)
+    subscribed: boolean
+    subscription_expires_at: string | null,
+    canceled: boolean,
+
+    // Relation
+    state: string | null
 }
 
 const props = defineProps<{
