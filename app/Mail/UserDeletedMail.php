5be6baf5892ab1b56ff526886eee926836b43efd
Lukas Korbitsch <korbitschl@gmail.com>
2026-02-11 17:22:33 +0100

    added strong tag to user deletion mail

--- app/Mail/UserDeletedMail.php
+++ app/Mail/UserDeletedMail.php
@@ -28,22 +28,22 @@ class UserDeletedMail extends Mailable implements ShouldQueue
                 'name' => $this->name,
                 'content' => new HtmlString('
                                             <p>
-                                            schade, dass du uns verlässt. Wir möchten uns herzlich bei dir bedanken,
-                                            dass du Teil unserer Plattform warst.
+                                                Schade, dass du uns verlässt. Wir möchten uns <strong>herzlich bei dir bedanken</strong>,
+                                                dass du Teil unserer Plattform warst.
                                             </p>
 
                                             <p>
-                                            Dein Account wurde erfolgreich gelöscht.
-                                            Alle aktiven Abonnements wurden selbstverständlich beendet.
+                                                Dein Account wurde <strong>erfolgreich gelöscht</strong>.<br>
+                                                Alle <strong>aktiven Abonnements</strong> wurden selbstverständlich beendet.
                                             </p>
 
                                             <p>
-                                            Falls du dich irgendwann entscheidest zurückzukommen,
-                                            bist du jederzeit wieder herzlich willkommen.
+                                                Falls du dich irgendwann entscheidest zurückzukommen,
+                                                bist du <strong>jederzeit wieder herzlich willkommen</strong>.
                                             </p>
 
                                             <p>
-                                            Wir wünschen dir alles Gute und viel Erfolg bei deinen Angelausflügen.
+                                                Wir wünschen dir <strong>alles Gute</strong> und viel Erfolg bei deinen Angelausflügen.
                                             </p>
                                             '),
             ]);
