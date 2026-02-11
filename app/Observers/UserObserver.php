bd2bda85c9b2dfb625ca8ba669586475129c6fd9
Lukas Korbitsch <korbitschl@gmail.com>
2026-02-11 17:16:17 +0100

    removed deletion of stripe customers

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -3,19 +3,13 @@
 namespace App\Observers;
 
 use App\Models\User;
-use Laravel\Cashier\Cashier;
 
 class UserObserver
 {
     public function deleting(User $user): void
     {
         if ($user->subscribed()) {
-
             $user->subscriptions->each(fn($sub) => $sub->cancelNow());
-            
-            // Stripe Customer komplett löschen
-            Cashier::stripe()->customers->delete($user->stripe_id);
-
         }
     }
 }
